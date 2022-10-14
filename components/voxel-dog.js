40561337a804831c9e5b1f1e347373134067e905
Mr.Wood <90689134+mrtonymu@users.noreply.github.com>
2022-10-15 07:41:44 +0800

    Update voxel-dog

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -12,7 +12,7 @@ const VoxelDog = () => {
   const refContainer = useRef()
   const [loading, setLoading] = useState(true)
   const refRenderer = useRef()
-  const urlDogGLB = (process.env.NODE_ENV === 'production' ? 'https://tonymumu.vercel.app/' : '') + '/dog.glb'
+  const urlDogGLB = `${process.env.NODE_ENV === 'production' ? 'https://tonymumu.vercel.app/' : ''}/dog.glb`
 
   const handleWindowResize = useCallback(() => {
     const { current: renderer } = refRenderer
